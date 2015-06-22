1f2329f10cfacd68fe7338eed947d529f3131f1a
kousiksatish <kousiksatish@gmail.com>
2015-06-23 01:01:59 +0530

    Basic View Template added

---
+++ Hostel-site/resources/views/index.blade.php
@@ -0,0 +1,26 @@
+@extends('base')
+
+@section('title')
+	Hostel Site - Home
+@stop
+
+@section('sidebar')
+
+<li>
+<a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+</li>
+<li>
+<a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+</li>
+<li>
+<a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+</li>
+<li>
+<a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+</li>
+
+@stop
+
+@section('content')
+	<h1>Content goes here!!!</h1>
+@stop
