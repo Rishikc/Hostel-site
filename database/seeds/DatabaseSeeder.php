3c40a3b1fb5486e24b7d4ecf627b25eccafea1c1
Vignesh <vigneshmanix@gmail.com>
2015-11-02 10:45:14 +0530

    fix seeder issue

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,3 +19,45 @@ class DatabaseSeeder extends Seeder
         Model::reguard();
     }
 }
+
+
+use App\Categories as Categories;
+
+
+class CategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    	Categories::insert([
+    			'name' => 'Carpentry',
+    			'url_name' => 'carpentry',
+    			'description' => 'Complaints related to all door, window, cupboards, hangers, etc.'
+    		]);
+    	Categories::insert([
+    			'name' => 'Plumbing',
+    			'url_name' => 'plumbing',
+    			'description' => 'Complaints related to all pipelines, drainage, etc'
+    		]);
+    	Categories::insert([
+    			'name' => 'House Keeping',
+    			'url_name' => 'house-keeping',
+    			'description' => 'Complaints related to hygiene and maintenance issues'
+    		]);
+    	Categories::insert([
+    			'name' => 'Drinking Water',
+    			'url_name' => 'drinking-water',
+    			'description' => 'Complaints related to RO problem'
+    		]);
+    	Categories::insert([
+    			'name' => 'Others',
+    			'url_name' => 'others',
+    			'description' => 'Complaints that don\'t fit into any of the above categories'
+    		]);
+    }
+}
