aacde0480807f2421f4687ced64a29591dca96f2
Kousik Satish <kousiksatish@gmail.com>
2016-04-11 23:10:26 +0530

    Change login redirect

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -61,7 +61,7 @@ class HomeController extends Controller
                 {
                 Session::put('roll_number',$username);
                 }
-                return Redirect::to(action('HomeController@complaint'));
+                return Redirect::to(action('HomeController@show'));
                 
             }
             else
